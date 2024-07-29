c44cca4714bdada7c4e42a050be5fd13ca1b7d11
Marcos Anjos <dev.marcosanjos@gmail.com>
2024-07-28 21:23:27 -0300

    Test Husky pre-commit hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,20 @@
-import globals from "globals";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import pluginPrettier from 'eslint-plugin-prettier'
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-];
+    {
+        languageOptions: {
+            globals: globals.browser,
+        },
+    },
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            'prettier/prettier': 'error',
+        },
+    },
+    prettier,
+    pluginPrettier,
+]
---
+++ index.js
@@ -0,0 +1,3 @@
+const teste = 'absjasjhba'
+
+console.log(teste)
